ac190fb7a63eb19fe99cd597f9cc443fa40cf8a9
yeremi777 <kurikarakenjou@gmail.com>
2022-09-08 00:03:05 +0700

    add new table

---
+++ database/migrations/2022_09_07_165430_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->integer('skema_pkm_id')->unsigned();
+            $table->string('title');
+            $table->string('pendanaan_dikti');
+            $table->string('pendanaan_pt');
+            $table->json('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
