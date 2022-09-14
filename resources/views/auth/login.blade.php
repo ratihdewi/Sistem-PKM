fb4a7a1798b0a9fd15afc34c423cd28a7068ee1d
yeremi777 <kurikarakenjou@gmail.com>
2022-09-14 16:01:34 +0700

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,8 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
+    <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js"
+        crossorigin="anonymous"></script>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
@@ -30,54 +31,7 @@
         <div class="content-wrapper">
             <div class="login-div">
                 <div class="form-container">
-                    {{-- <form action="/login" method="post">
-                        @csrf
-                        <div class="form">
-                            <div class="user-name" style="margin-bottom: 10px;"> Username</div>
-                            <div class="username">
-                                <svg class="svg-icon" viewBox="0 0 20 20">
-                                    <path
-                                        d="M12.075,10.812c1.358-0.853,2.242-2.507,2.242-4.037c0-2.181-1.795-4.618-4.198-4.618S5.921,4.594,5.921,6.775c0,1.53,0.884,3.185,2.242,4.037c-3.222,0.865-5.6,3.807-5.6,7.298c0,0.23,0.189,0.42,0.42,0.42h14.273c0.23,0,0.42-0.189,0.42-0.42C17.676,14.619,15.297,11.677,12.075,10.812 M6.761,6.775c0-2.162,1.773-3.778,3.358-3.778s3.359,1.616,3.359,3.778c0,2.162-1.774,3.778-3.359,3.778S6.761,8.937,6.761,6.775 M3.415,17.69c0.218-3.51,3.142-6.297,6.704-6.297c3.562,0,6.486,2.787,6.705,6.297H3.415z">
-                                    </path>
-                                </svg>
-                                <input class="input @error('username') is-invalid @enderror" type="text"
-                                    placeholder="Username" name="username" required>
-                                @error('username')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-
-                            <div class="pass-name" style="margin-bottom: 10px;"> Password</div>
-                            <div class="password">
-                                <svg class="svg-icon" viewBox="0 0 20 20">
-                                    <path
-                                        d="M17.308,7.564h-1.993c0-2.929-2.385-5.314-5.314-5.314S4.686,4.635,4.686,7.564H2.693c-0.244,0-0.443,0.2-0.443,0.443v9.3c0,0.243,0.199,0.442,0.443,0.442h14.615c0.243,0,0.442-0.199,0.442-0.442v-9.3C17.75,7.764,17.551,7.564,17.308,7.564 M10,3.136c2.442,0,4.43,1.986,4.43,4.428H5.571C5.571,5.122,7.558,3.136,10,3.136 M16.865,16.864H3.136V8.45h13.729V16.864z M10,10.664c-0.854,0-1.55,0.696-1.55,1.551c0,0.699,0.467,1.292,1.107,1.485v0.95c0,0.243,0.2,0.442,0.443,0.442s0.443-0.199,0.443-0.442V13.7c0.64-0.193,1.106-0.786,1.106-1.485C11.55,11.36,10.854,10.664,10,10.664 M10,12.878c-0.366,0-0.664-0.298-0.664-0.663c0-0.366,0.298-0.665,0.664-0.665c0.365,0,0.664,0.299,0.664,0.665C10.664,12.58,10.365,12.878,10,12.878">
-                                    </path>
-                                </svg>
-
-                                <input id="password" class="input @error('password') is-invalid @enderror"
-                                    type="password" placeholder="Password" name="password" required>
-                                @error('password')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-
-                            <div class="options">
-                                <div class="forget-password">
-                                    <div class="pass-link"><a href="#">Forgot password?</a></div>
-                                </div>
-                            </div>
-                            <div class="button-btn">
-                                <button type="submit" class="signin-btn">SIGN IN</button>
-                            </div>
-                        </div>
-                    </form> --}}
-
-                    <form action="/login" method="post">
+                    <form action="{{ route('login.post') }}" method="post">
                         @csrf
                         <div class="form-group">
                             <label class="control-label" for="username">
@@ -87,7 +41,9 @@
                                 <div class="input-group-prepend">
                                     <div class="input-group-text"><i class="fa fa-user"></i></div>
                                 </div>
-                                <input type="text" class="form-control" id="inlineFormInputGroupUsername" placeholder="Username" aria-describedby="inputGroupPrepend3" required>
+                                <input type="text" class="form-control @error('username') is-invalid @enderror"
+                                    id="inlineFormInputGroupUsername" placeholder="Username" name="username"
+                                    aria-describedby="inputGroupPrepend3">
                             </div>
                         </div>
                         <div class="form-group">
@@ -98,9 +54,13 @@
                                 <div class="input-group-prepend">
                                     <div class="input-group-text"><i class="fa fa-key"></i></div>
                                 </div>
-                                <input type="password" class="form-control" id="inlineFormInputGroupUsername" placeholder="Password" required>
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text"><i class="fa fa-eye-slash" aria-hidden="true"></i></div>
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    id="inlineFormInputGroupUsername" placeholder="Password" name="password">
+                                <div class="input-group-append" onmousedown="showPassword('#password', '#eye')"
+                                    onmouseup="hidePassword('#password', '#eye')">
+                                    <div class="input-group-text"><i class="fa fa-eye-slash"></i>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -112,7 +72,7 @@
                         <div class="text-center mt-4">
                             <button type="submit" class="btn btn-primary">Sign In</button>
                         </div>
-                      </form>
+                    </form>
                 </div>
             </div>
         </div>
@@ -123,9 +83,8 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"
+        integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
     </script>
@@ -138,7 +97,15 @@
     <!-- Date Range Picker -->
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 
-    @yield('extra_js')
+    <script>
+        function showPassword() {
+            $('#password').attr('type', 'text');
+        }
+
+        function hidePassword() {
+            $('#password').attr('type', 'password');
+        }
+    </script>
 </body>
 
 </html>
