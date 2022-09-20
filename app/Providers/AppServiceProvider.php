5c09bc93da5cb78630d795e9522fe195c95f897f
yeremi777 <kurikarakenjou@gmail.com>
2022-09-20 23:52:34 +0700

    setup views based on user role

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Gate::define('admin', fn (User $user) => $user->role_id === 1);
+        Gate::define('dosen', fn (User $user) => $user->role_id === 2);
+        Gate::define('mahasiswa', fn (User $user) => $user->role_id === 3);
     }
 }
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -8,24 +8,65 @@
                         <div class="sb-nav-link-icon"></div>
                         Beranda
                     </a>
-                    <a class="nav-link" href="/proposal">
-                        <div class="sb-nav-link-icon"></div>
-                        Pengajuan Proposal
-                    </a>
-                    <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                        data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                        <div class="sb-nav-link-icon"></div>
-                        Pelaksanaan Kegiatan
-                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                    </a>
-                    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
-                        data-bs-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="layout-static.html">Laporan Kemajuan</a>
-                            <a class="nav-link" href="layout-sidenav-light.html">Laporan Akhir</a>
-                        </nav>
-                    </div>
+                    @can('admin')
+                        <a class="nav-link" href="#">
+                            <div class="sb-nav-link-icon"></div>
+                            Daftar Usulan
+                        </a>
+                        <a class="nav-link" href="#">
+                            <div class="sb-nav-link-icon"></div>
+                            Daftar Usulan Didanai
+                        </a>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <div class="sb-nav-link-icon"></div>
+                            Data Pendukung
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="#">Data Mahasiswa</a>
+                                <a class="nav-link" href="#">Dosen Pendamping</a>
+                                <a class="nav-link" href="#">Pengaturan Reviewer</a>
+                            </nav>
+                        </div>
+                        <a class="nav-link" href="#">
+                            <div class="sb-nav-link-icon"></div>
+                            Dokumen
+                        </a>
+                        <a class="nav-link" href="#">
+                            <div class="sb-nav-link-icon"></div>
+                            Pengaturan Konten
+                        </a>
+                    @endcan
+                    @can('dosen')
+                        <a class="nav-link" href="#">
+                            <div class="sb-nav-link-icon"></div>
+                            Review Proposal
+                        </a>
+                    @endcan
+                    @can('mahasiswa')
+                        <a class="nav-link" href="/proposal">
+                            <div class="sb-nav-link-icon"></div>
+                            Pengajuan Proposal
+                        </a>
+                        <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
+                            data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+                            <div class="sb-nav-link-icon"></div>
+                            Pelaksanaan Kegiatan
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                        </a>
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="#">Laporan Kemajuan</a>
+                                <a class="nav-link" href="#">Laporan Akhir</a>
+                            </nav>
+                        </div>
+                    @endcan
                 </div>
+
                 <div class="mt-4 text-center">
                     <form action="{{ route('logout.post') }}" method="post">
                         @csrf
