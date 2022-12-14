440d8b8c4877172b10f972140c9c4173d9499acd
= <ugyparaptughessa@gmail.com>
2022-12-14 09:32:01 +0700

    navbar

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="navbar navbar-light" style="z-index: 9999">
+<nav class="navbar navbar-light" style="z-index: 100">
     <div class="nav">
         <img class="logo" src="{{ asset('assets/img/univ_pertamina_logo.png') }}" alt="" height="120px">
         <span class="navbar-text"> SISTEM PROGRAM KREATIVITAS MAHASISWA <br /> UNIVERSITAS PERTAMINA </span>
